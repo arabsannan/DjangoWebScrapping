4c63fafd7834bb6ad3dedc0d7207c45141349fb9
aaannan <a.affran-annan@myzeepay.com>
2023-05-30 17:35:08 +0000

    comments changed

--- capture/views.py
+++ capture/views.py
@@ -9,13 +9,13 @@ import json
 
 load_dotenv()  # Load environment variables from .env file
 
-# Auth
+# API credentials
 api_key = os.getenv('API_KEY')
 api_secret_key = os.getenv('API_SECRET_KEY')
 access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-# setting up twitter APIs
+# Authenticate with Twitter API
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
